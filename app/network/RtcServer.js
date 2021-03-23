83692d09dedd113b4ddd01b942f9b0de7f30166a
Sean Morris <sean@seanmorr.is>
2021-03-23 17:38:17 -0400

    Abstracting RTC connection

--- app/network/RtcServer.js
+++ app/network/RtcServer.js
@@ -1,56 +1,29 @@
-import { Task } from 'subspace-console/Task';
-import { Tag } from 'curvature/base/Tag';
+import { Mixin } from 'curvature/base/Mixin';
+import { EventTargetMixin } from 'curvature/mixin/EventTargetMixin';
 
-const Accept = Symbol('accept');
-
-export class RtcServer extends Task
+export class RtcServer extends Mixin.with(EventTargetMixin)
 {
-	static helpText = 'RTC Server.';
-	static useText  = '';
-
-	title     = 'RTC Server Task';
-	connected = false;
-
-	init()
+	constructor(rtcConfig)
 	{
-		const rtcConfig = {
-			// iceServers: [
-			// 	{urls: 'stun:stun1.l.google.com:19302'},
-			// 	{urls: 'stun:stun2.l.google.com:19302'}
-			// ]
-		};
+		super();
 
 		this.peerServer = new RTCPeerConnection(rtcConfig);
 
-		this.peerServer.addEventListener('icecandidate', event => {
-			if(!event.candidate)
-			{
-				return;
-			}
+		this.answerToken = new Promise(accept => {
+			this.peerServer.addEventListener('icecandidate', event => {
+				if(!event.candidate)
+				{
+					return;
+				}
 
-			let localDescription = JSON.stringify(this.peerServer.localDescription);
-
-			this.print('Server Answer');
-
-			this.print(localDescription);
-
-			const offerTag = new Tag('<textarea style = "display:none">');
-
-			offerTag.innerText = localDescription;
-
-			document.body.append(offerTag.node);
-
-			offerTag.select();
-
-			document.execCommand("copy");
-
-			offerTag.node.remove();
-		});
+				accept(this.peerServer.localDescription);
+			});
+		})
 
 		this.peerServer.addEventListener('iceconnectionstatechange', () => {
 			let state = this.peerServer.iceConnectionState;
 
-			this.print(`RTC state: ${state}`);
+			console.log(`RTC state: ${state}`);
 		});
 
 		this.peerServer.addEventListener('datachannel', event => {
@@ -58,80 +31,58 @@ export class RtcServer extends Task
 			this.peerServerChannel = event.channel;
 
 			this.peerServerChannel.addEventListener('open', () => {
-				this.print('Remote peer client accepted!');
-
 				this.connected = true;
+
+				const openEvent = new CustomEvent('open', {detail: event.data });
+
+				openEvent.originalEvent = event;
+
+				console.log('Remote peer client accepted!');
+
+				this.dispatchEvent(openEvent);
+
 			});
 
 			this.peerServerChannel.addEventListener('close', () => {
-				this.print('Peer reset connection.');
+				const closeEvent = new CustomEvent('close', {detail: event.data });
 
-				this[Accept]();
-			});
+				closeEvent.originalEvent = event;
 
-			this.finally(()=>{
-				if(this.peerServerChannel)
-				{
-					this.print('Terminating connection...');
-					this.peerServerChannel.close();
-				}
+				this.dispatchEvent(closeEvent);
+
+				console.log('Peer reset connection.');
 			});
 
 			this.peerServerChannel.addEventListener('message', event => {
-				this.print(`> ${event.data}`);
+				console.log(`> ${event.data}`);
 				console.log(event);
+				const messageEvent = new CustomEvent('message', {detail: event.data });
+
+				messageEvent.originalEvent = event;
+
+				this.dispatchEvent(messageEvent);
 			});
 
 		});
-
-		this.printErr(`Please supply SDP offer string.`);
-
-		return new Promise(accept => {
-			this[Accept] = accept;
-		});
 	}
 
-	main(input)
+	send(input)
 	{
-		if(!input)
-		{
-			return;
-		}
-
-		if(!this.connected)
-		{
-			this.answer(input);
-			return;
-		}
-
-		this.print(`< ${input}`);
 		this.peerServerChannel.send(input);
 	}
 
-	answer(offerString)
+	close()
 	{
-		const offer = JSON.parse(offerString);
+		this.peerServerChannel.close()
+	}
 
+	answer(offer)
+	{
 		this.peerServer.setRemoteDescription(offer);
 
 		this.peerServer.createAnswer(
-			(answer) => {
-
-				this.peerServer.setLocalDescription(answer);
-
-			}, (error) => {
-
-				console.error(error);
-
-				this.printErr(`Unexpected exception.`);
-
-				this[Accept]();
-			}
+			answer => this.peerServer.setLocalDescription(answer)
+			, error => console.error(error)
 		);
-	}
-
-	done()
-	{
-
 	}
 }
---
+++ app/network/RtcServerTask.js
@@ -0,0 +1,107 @@
+import { Task } from 'subspace-console/Task';
+import { Tag } from 'curvature/base/Tag';
+
+import { RtcServer } from './RtcServer';
+
+const Accept = Symbol('accept');
+
+export class RtcServerTask extends Task
+{
+	static helpText = 'RTC Server.';
+	static useText  = '';
+
+	title     = 'RTC Server Task';
+	connected = false;
+
+	init()
+	{
+		this.server = new RtcServer({
+			// iceServers: [
+			// 	{urls: 'stun:stun1.l.google.com:19302'},
+			// 	{urls: 'stun:stun2.l.google.com:19302'}
+			// ]
+		});
+
+		this.finally(()=>{
+			this.print('Terminating connection...');
+			this.server.close();
+		});
+
+		this.server.addEventListener('open', () => {
+			this.print('Remote peer client accepted!');
+		});
+
+		this.server.addEventListener('close', () => {
+			this.print('Peer reset connection.');
+		});
+
+		this.server.addEventListener('message', event => {
+			this.print(`> ${event.detail}`);
+		});
+
+		this.server.answerToken.then(token => {
+			this.print('Server Answer');
+
+			const tokenString  = JSON.stringify(token);
+			const encodedToken = `s3ktp://accept/${btoa(tokenString)}`;
+
+			this.print(encodedToken);
+
+			const answerTag = new Tag('<textarea style = "display:none">');
+
+			answerTag.innerText = encodedToken;
+
+			document.body.append(answerTag.node);
+
+			answerTag.select();
+
+			document.execCommand("copy");
+
+			answerTag.node.remove();
+		});
+
+		this.printErr(`Please supply SDP offer string.`);
+
+		return new Promise(accept => {
+			this[Accept] = accept;
+		});
+	}
+
+	main(input)
+	{
+		if(!input)
+		{
+			return;
+		}
+
+		if(!this.server.connected)
+		{
+			this.answer(input);
+			return;
+		}
+
+		this.print(`< ${input}`);
+		this.server.send(input);
+	}
+
+	answer(offerString)
+	{
+		const isEncoded = /^s3ktp:\/\/request\/(.+)/.exec(offerString);
+
+		console.log(isEncoded);
+
+		if(isEncoded)
+		{
+			offerString = atob(isEncoded[1]);
+		}
+
+		const offer = JSON.parse(offerString);
+
+		this.server.answer(offer);
+	}
+
+	done()
+	{
+
+	}
+}
