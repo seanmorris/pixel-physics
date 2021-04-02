05ad5e8ebc205f29751912434b2fd9553585c468
Sean Morris <sean@seanmorr.is>
2021-04-02 07:38:15 -0400

    props

---
+++ docs/map/lz-props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="lz-props" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="lz-props.png" width="256" height="256"/>
+</tileset>
