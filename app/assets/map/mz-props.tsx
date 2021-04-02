2078e3e15c870a97f4d5d06b92aec6d494ac2d6b
Sean Morris <sean@seanmorr.is>
2021-04-02 07:37:36 -0400

    props

---
+++ app/assets/map/mz-props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="props" tilewidth="32" tileheight="32" tilecount="150" columns="10">
+ <image source="../Sonic/tiles/marble/props.png" width="320" height="480"/>
+</tileset>
