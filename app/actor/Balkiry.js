9db6547d856ae87f234d80807411ebe313b05a9b
Sean Morris <640101+seanmorris@users.noreply.github.com>
2022-05-17 19:28:50 -0400

    Manic harbor files.

---
+++ app/actor/Balkiry.js
@@ -0,0 +1,92 @@
+import { Flickie } from './Flickie';
+
+import { Mixin } from 'curvature/base/Mixin';
+import { Tag } from 'curvature/base/Tag';
+import { PointActor } from './PointActor';
+
+import { SkidDust } from '../behavior/SkidDust';
+import { CanPop } from '../mixin/CanPop';
+
+import { Explosion } from '../actor/Explosion';
+import { Projectile } from '../actor/Projectile';
+
+export class Balkiry extends Mixin.from(PointActor, CanPop)
+{
+	constructor(...args)
+	{
+		super(...args);
+
+		this.args.type      = 'actor-item actor-balkiry';
+
+		this.args.animation = 'standing';
+
+		this.args.accel     = 0.1;
+		this.args.decel     = 0.5;
+
+		this.args.gSpeedMax = 5;
+		this.args.jumpForce = 5;
+		this.args.gravity   = 0.5;
+
+		this.args.width     = 32;
+		this.args.height    = 16;
+
+		this.willStick = false;
+		this.stayStuck = false;
+
+		this.args.float = -1;
+
+		this.aiming = false;
+	}
+
+	onRendered()
+	{
+		super.onRendered();
+
+		this.flame = new Tag('<div class = "balkiry-flame">');
+
+		this.sprite.appendChild(this.flame.node);
+
+		this.box.setAttribute('data-animation', 'standing');
+	}
+
+	update()
+	{
+		// this.args.facing  = 'right';
+		this.args.facing  = 'left';
+
+		if(this.args.xSpeed > -12)
+		{
+			this.args.xSpeed -= 0.2;
+		}
+
+		this.args.ySpeed  = 0;
+		this.args.falling = true;
+		this.args.flying  = true;
+
+		super.update();
+	}
+
+	effect(other)
+	{
+		super.effect(other);
+
+		// this.viewport.spawn.add({object:new Flickie({
+		// 	x: this.args.x,
+		// 	y: this.args.y,
+		// })});
+	}
+
+	wakeUp()
+	{
+	}
+
+	sleep()
+	{
+		// this.args.x = this.def.get('x');
+		// this.args.y = this.def.get('y');
+	}
+
+	get solid() { return false; }
+	// get controllable() { return true; }
+	get isEffect() { return false; }
+}
---
+++ app/actor/Booster.js
@@ -0,0 +1,70 @@
+import { PointActor } from './PointActor';
+
+export class Booster extends PointActor
+{
+	constructor(...args)
+	{
+		super(...args);
+
+		this.args.type = 'actor-item actor-booster';
+
+		this.args.xMax = this.args.xMax ?? 24;
+		this.args.yMax = this.args.yMax ?? 24;
+
+		this.args.width  = 64;
+		this.args.height = 345;
+
+		this.args.float = -1;
+	}
+
+	update()
+	{
+		if(!this.viewport)
+		{
+			return;
+		}
+
+		super.update();
+
+		if(this.args.launched && Math.abs(this.args.xSpeed) < Math.abs(this.args.xMax))
+		{
+			this.args.xSpeed += 0.5 * Math.sign(this.args.xMax);
+		}
+
+		if(this.args.launched && Math.abs(this.args.ySpeed) < Math.abs(this.args.yMax))
+		{
+			this.args.ySpeed -= 5 * Math.sign(this.args.yMax);
+		}
+
+		if(this.args.launched)
+		{
+			this.args.falling = true;
+		}
+
+		if(this.args.launched && this.args.xSpeed === 0 && this.args.ySpeed === 0)
+		{
+			// this.explode();
+		}
+	}
+
+	activate()
+	{
+		console.log('GO!');
+
+		this.args.active = true;
+
+		this.viewport.auras.add(this);
+
+		this.args.launched = true;
+
+		if(this.args.xMax)
+		{
+			this.args.xSpeed += 0.5;
+		}
+
+		if(this.args.yMax)
+		{
+			this.args.ySpeed -= 0.5;
+		}
+	}
+}
